313115e8bb41db235938240f048d8414cbb353e6
Tristan Dardoize <tristan.dardoize@depinfonancy.net>
2019-02-15 11:44:20 +0000

    Stratege files added

---
+++ src/Stratege/Stratege.cc
@@ -0,0 +1,92 @@
+#include "Stratege.h"
+#include "AutoPilotPlugin.h"
+#include "MAVLinkProtocol.h"
+#include "UAS.h"
+#include "QGCApplication.h"
+#include "FollowMe.h"
+#include "QGroundControlQmlGlobal.h"
+#include "ParameterManager.h"
+#include "SettingsManager.h"
+#include "QGCCorePlugin.h"
+#include "QGCOptions.h"
+
+
+#include <QQmlEngine>
+
+VehicleAttribut::VehicleAttribut()
+{
+    _role = -1;
+    _lonLatAltCoord = QGeoCoordinate();
+    _hdg = UINT16_MAX;  //If we don't know, we set to this Value                        https://mavlink.io/en/messages/common.html#GLOBAL_POSITION_INT
+    _targetLonLatAltCoord = QGeoCoordinate();
+    _mainServo = QList<int16_t>();
+    _auxServo = QList<int16_t>();
+}
+
+
+
+Stratege::Stratege(QGCApplication* app, QGCToolbox* toolbox): QGCTool(app, toolbox)
+{
+    _abortMission = false;
+    _startMission = false;
+    _time = QTime();
+    _time.start();
+
+
+//    _vehicleMap = new QMap<Vehicle*, VehicleAttribut*>();
+}
+
+
+void Stratege::abortMission()
+{
+    _startMission = false;
+    _abortMission = true; //eventually, instead of having a variable here implement the solution right here
+    qDebug() << "Mission Aborted";
+    qDebug() << "Time in minutes: " << _time.minute();
+
+}
+
+
+void Stratege::startMission()
+{
+    _startMission = true;
+    _time.restart();
+    qDebug() << "Mission Started";
+}
+
+
+void Stratege::updateData(mavlink_message_t& message)
+{
+    if (_startMission == true)
+    {
+        //TODO
+        /*
+         * Receives a mavlink_message_t and update the member variables
+         */
+
+
+        //If we have started the mission, then this should appears in the console
+        Vehicle* activeVehicle = _toolbox->multiVehicleManager()->activeVehicle();
+        qDebug() << "Confirmation of active vehicle: " << activeVehicle->id();
+
+    }
+}
+
+
+void Stratege::_addedVehicle(Vehicle* vehicle)
+{
+    //TODO
+    /*
+     * Increase the size of the member variables (List or something...)
+     */
+}
+
+void Stratege::_removedVehicle(Vehicle* vehicle)
+{
+    //TODO
+    /*
+     * Decrease the size of the member variables (List or something...)
+     */
+}
+
+
---
+++ src/Stratege/Stratege.h
@@ -0,0 +1,150 @@
+#ifndef Stratege_H
+#define Stratege_H
+
+#include "Vehicle.h"
+#include "QGCMAVLink.h"
+#include "QmlObjectListModel.h"
+#include "QGCToolbox.h"
+#include "QGCLoggingCategory.h"
+
+
+class FirmwarePluginManager;
+class QGCApplication;
+class MAVLinkProtocol;
+
+
+//Zones
+#define DEFENSE_ZONE     0
+#define ATTACK_ZONE      1
+#define NEUTRAL_ZONE     2
+#define OUTSIDE_ZONE     3
+
+
+//Roles
+#define DEFENSE_UAV     0
+#define ATTACK_UAV      1
+#define DEFENSE_UGV     2
+
+//Value of PWM
+#define PWM_OPEN_SERVO  1800        //To change to real value
+#define PWM_CLOSE_SERVO 1100        //To change to real value
+#define MOTOR_MAX_PWM   1800        //To change to real value
+
+//Time values for Strategy
+#define START_TIME_M        0
+#define STOP_TIME_M         10
+#define GO_ATTACK_TIME_M    8       //To change to real value
+
+
+class VehicleAttribut
+{
+public:
+      VehicleAttribut();                                                                                        // Implementation in Stratege.cc
+
+//      bool isInZone(int zone);                                                                                  // Implementation in Stratege.cc
+//      bool isHookOpened();                                                                                      // Implementation in Stratege.cc
+//      bool isThrottleTooLarge();                                                                                // Implementation in Stratege.cc
+
+
+      void setRole(int role) { _role = role; }
+//      void setlonLatAltCoord(float north, float east, float down);                                              // Implementation in Stratege.cc        //Need to convert from NED to Lon Lat Alt
+      void setHdg(uint16_t hdg) { _hdg = hdg; }
+//      void setTargetLonLatAltCoord(int32_t lat, int32_t lon, float alt);                                        // Implementation in Stratege.cc
+//      void setMainServo(int16_t servo1_raw = 0, int16_t servo2_raw = 0, int16_t servo3_raw = 0, int16_t servo4_raw = 0, int16_t servo5_raw = 0, int16_t servo6_raw = 0, int16_t servo7_raw = 0, int16_t servo8_raw = 0);      // Implementation in Stratege.cc
+//      void setAuxServo(int16_t servo1_raw = 0, int16_t servo2_raw = 0, int16_t servo3_raw = 0, int16_t servo4_raw = 0, int16_t servo5_raw = 0, int16_t servo6_raw = 0, int16_t servo7_raw = 0, int16_t servo8_raw = 0);       // Implementation in Stratege.cc
+
+
+      int role() { return _role; }
+      QGeoCoordinate lonLatAltCoord() { return _lonLatAltCoord; }
+      uint16_t hdg() { return _hdg; }
+      QGeoCoordinate targetLonLatAltCoord() { return _targetLonLatAltCoord; }
+      QList<int16_t> mainServo() { return _mainServo; }
+      QList<int16_t> auxServo() { return _auxServo; }
+
+
+private:
+
+      //Member Variables
+
+      //Vehicle role
+      int _role;
+
+      //Lon/Lat/Alt coordinates
+      QGeoCoordinate _lonLatAltCoord;
+
+      //Heading
+      uint16_t _hdg;
+
+      //Coordinates of the last enemy spotted by this vehicle
+      QGeoCoordinate _targetLonLatAltCoord;
+
+      //Servo Information, PPM Modulation
+      QList<int16_t> _mainServo;
+      QList<int16_t> _auxServo;
+};
+
+
+
+class Stratege : public QGCTool
+{
+    Q_OBJECT
+
+public:
+    Stratege(QGCApplication* app, QGCToolbox* toolbox);
+
+
+    //TODO: correctly initialize the coordinates describing the area of interest
+
+    //Geofence Information                                                                          //To modify: set the real coordinates for the competition. Hypothesis: zones are rectangles
+
+//    //Area of interest: DEFENSE + NEUTRAL + ATTACK
+//    static const QGeoCoordinate globalTopLeft = QGeoCoordinate();                                   //Hint: to access to these variables, use Stratege::globalTopLeft
+//    static const QGeoCoordinate globalTopRight = QGeoCoordinate();
+//    static const QGeoCoordinate globalBottomLeft = QGeoCoordinate();
+//    static const QGeoCoordinate globalBottomRight = QGeoCoordinate();
+
+//    //Defense zone coordinates
+//    static const QGeoCoordinate defenseTopLeft = QGeoCoordinate();
+//    static const QGeoCoordinate defenseTopRight = QGeoCoordinate();
+//    static const QGeoCoordinate defenseBottomLeft = QGeoCoordinate();
+//    static const QGeoCoordinate defenseBottomRight = QGeoCoordinate();
+
+//    //Attack zone coordinates
+//    static const QGeoCoordinate attackTopLeft = QGeoCoordinate();
+//    static const QGeoCoordinate attackTopRight = QGeoCoordinate();
+//    static const QGeoCoordinate attackBottomLeft = QGeoCoordinate();
+//    static const QGeoCoordinate attackBottomRight = QGeoCoordinate();
+
+//    //Neutral zone coordinates
+//    static const QGeoCoordinate neutralTopLeft = QGeoCoordinate();
+//    static const QGeoCoordinate neutralTopRight = QGeoCoordinate();
+//    static const QGeoCoordinate neutralBottomLeft = QGeoCoordinate();
+//    static const QGeoCoordinate neutralBottomRight = QGeoCoordinate();
+
+public slots:
+    void abortMission();
+    void startMission();
+    void updateData(mavlink_message_t& message);
+
+private slots:
+    void _addedVehicle(Vehicle* vehicle); //Modify dynamically the size of the member variables
+    void _removedVehicle(Vehicle* vehicle);
+
+private:
+    void _mtFiltering();                                                                            //To modify: return list of QGeoGoordinates representing true enemy position. Return also speed indication-> not this type then ??
+    void _taskControl();
+
+    //Member variables
+
+    bool _abortMission;
+    bool _startMission;
+
+    QTime _time;
+
+//    QMap<Vehicle*, VehicleAttribut*>* _vehicleMap;                                                   //Assign to each Vehicle* an associated VehicleAttribut*
+
+};
+
+
+
+#endif
